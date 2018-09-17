e4e3dc04a5815aba0dba31b623e810d2ac85f090
Adrian Hintze <hintze.adrian@gmail.com>
2018-09-17 23:14:30 +0100

    test

--- src/Assistant.ts
+++ src/Assistant.ts
@@ -5,7 +5,7 @@ class Assistant {
         this._client = new Compute({});
         const url = `https://embeddedassistant.googleapis.com/dns/v1/projects/${this._projectId}`;
         const res = await this._client.request({ url });
-        console.log(res.data);
+        console.log(res);
     }
 
     private _client: any;
